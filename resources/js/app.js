b2eabc90eef0cf2cfca74153cf5d272cdff1d830
Krunal Shah <shahkrunal7091@gmail.com>
2021-02-18 20:35:04 +0530

    feat: setup react

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
-require('./bootstrap');
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+require('./bootstrap')
+
+const App = () => <div>123</div>
+
+ReactDOM.render(<App />, document.getElementById('app'))
