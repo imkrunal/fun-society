46c86feefe9757eefedcf46e035301fb7ec34a58
Krunal Shah <shahkrunal7091@gmail.com>
2021-02-18 20:44:18 +0530

    feat: add material ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,29 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from '@material-ui/styles'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { teal } from '@material-ui/core/colors'
+import { Button } from '@material-ui/core'
 require('./bootstrap')
 
-const App = () => <div>123</div>
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: teal[500],
+    },
+    secondary: {
+      main: '#11cb5f',
+    },
+  },
+})
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Button color="primary" variant="contained">
+      Primary
+    </Button>
+  </ThemeProvider>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'))
